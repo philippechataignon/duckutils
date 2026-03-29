646208d4d4f650bb3db51d9b37029408828b904e
Philippe Chataignon <philippe.chataignon@gmail.com>
2026-03-29 11:11:33 +0200

    Remove refresh_secret

--- R/utils.R
+++ R/utils.R
@@ -10,14 +10,3 @@ tempname <- function(prefix = "temp") {
     sample(10000:99999, 1)
   )
 }
-
-#' Refresh S3 credentials
-#' @param conn DuckDB connection
-#' @details
-#' This is a placeholder function. Override it with your own implementation
-#' if your workflow requires refreshing S3 credentials (e.g., short-lived
-#' STS tokens). By default it does nothing.
-#' @keywords internal
-refresh_secret <- function(conn) {
-  invisible(NULL)
-}
